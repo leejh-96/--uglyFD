6f1364b4752645d1798d571d7fa3c34bacd6590d
leejh-96 <sbxl12@naver.com>
2023-02-24 17:18:24 +0900

    uglyfd-board vo

---
+++ uglyFD-최종병합/src/main/java/com/uglyfd/board/model/vo/Reply.java
@@ -0,0 +1,28 @@
+package com.uglyfd.board.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reply {
+	private int no;
+	
+	private int bcNo;
+	
+	private int boardNo;
+	
+	private int writerNo;
+	
+	private String writerId;
+	
+	private String content;	
+	
+	private Date createDate;
+	
+	private Date updateDate;
+}
